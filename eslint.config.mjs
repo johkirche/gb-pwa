d280490bc397fa2e9b7b6bab71c7484b4fb8ca41
Frederic Abraham <mail@fabraham.dev>
2025-06-21 11:15:06 +0200

    feat: configure eslint for vue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,13 +3,35 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  ...pluginVue.configs["flat/recommended"],
   eslintConfigPrettier,
   {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node, // if you need Node.js globals
+      },
+    },
+  },
+  {
+    files: ["*.vue", "**/*.vue"],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        extraFileExtensions: [".vue"],
+        sourceType: "module",
+      },
+    },
+  },
+  {
     ignores: [
+      "*.d.ts",
       "dist/*",
       "dev-dist/*",
       "node_modules/*",
